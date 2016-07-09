1d2a9aba7e7c1bf3ef79cc976d3eedb0cf3e2327
Raf Smits <raf_smits@hotmail.com>
2016-07-09 19:15:09 +0200

    Add MongoDB logic for individual sports

--- server/app.js
+++ server/app.js
@@ -11,6 +11,10 @@ app.use( express.static(__dirname + '/../client'));
 app.get('/sports', (request, response) => {
   let sports = mongoUtil.sports();
   sports.find().toArray((err, docs) => {
+    if (err) {
+      response.sendStatus(400);
+    }
+
     console.log(JSON.stringify(docs));
     let sportNames = docs.map((sport) => sport.name);
     response.json(sportNames);
@@ -19,22 +23,17 @@ app.get('/sports', (request, response) => {
 
 app.get('/sports/:name', (request, response) => {
   let sportName = request.params.name;
-  console.log("Sportname: ", sportName);
+  let sports = mongoUtil.sports();
 
-  let sport = {
-    "name": "Cycling",
-    "goldMedals": [{
-      "division": "Men's Sprint",
-      "country": "UK",
-      "year": 2012
-    }, {
-      "division": "Women's Sprint",
-      "country": "Australia",
-      "year": 2012
-    }]
-  }
-  
-  response.json(sport);
+  sports.find({name: sportName}).limit(1).next((err, doc) => {
+    if (err) {
+      response.sendStatus(400);
+    }
+      console.log("Sport: ", doc);
+      response.json(doc);
+  });
+
+
 });
 
 app.listen(8181, () => console.log('Listening on 8181...'));
